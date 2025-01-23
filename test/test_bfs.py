e8633e5e2231d333b22784d82b48f9c68d1113e0
Iryna Irkliyenko <iryna.irklyenko@ucsf.edu>
2025-01-23 12:37:20 -0800

    test_bfs

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,5 +1,6 @@
 # write tests for bfs
 import pytest
+import networkx as nx
 from search import graph
 
 def test_bfs_traversal():
@@ -12,13 +13,28 @@ def test_bfs_traversal():
     """
     input_graph = "data/tiny_network.adjlist"
 
+    '''test_graph = nx.Graph()
+    test_graph.add_nodes_from(["a", "b", "c", "d", "e", "f", "g", "h"])
+    test_graph.add_edges_from([("a", "b"),
+    ("a", "c"),
+    ("b", "d"),
+    ("c", "e"),
+    ("c", "f"),
+    ("c", "g"),
+    ("g", "h")])
+
+    test = graph.Graph(test_graph)
+    st = "1"
+    test_res = test.bfs(st)
+    print(test_res)'''
     # Create the graph object
     g = graph.Graph(input_graph)
     start_node = "Luke Gilbert"
 
     res = g.bfs(start_node)
-
-    assert len(res) == 30, 'bfs func does not work'
+    print(res)
+    assert len(res) == 30, f"Expected 30 nodes, but got {len(res)}"
+     
 
 
 def test_bfs():
@@ -44,7 +60,35 @@ def test_bfs():
     res_pos = g.bfs(node_a, node_b)
     res_neg = g.bfs(node_a, node_c)
  
-    assert res_pos is not None, "Not connected"
-    assert res_neg is None, "Someting is wrong"
+    assert res_pos is not None, f"Expected a path between {node_a} and {node_b}, but got None."
+    assert res_neg is None, f"Expected None for disconnected nodes, but got {res_neg}."
 
+
+
+def test_bfs_edge_cases():
+
+    input_graph = "data/tiny_network.adjlist"
+
+    # Start node
+    start_node = "Nadav Ahituv"
+
+    # Edge case: empty graph
+    empty_graph = nx.Graph()
+    empty_graph = graph.Graph(empty_graph)
+
+    with pytest.raises(ValueError, match="Graph is empty"):
+        empty_graph.bfs(start_node)
+
+    ## Create the graph object
+    g = graph.Graph(input_graph)
+
+    # Edge case: Start node does not exist
+    with pytest.raises(ValueError):
+        g.bfs(start="Nonexistent Node")
+
+    # Edge case: End node does not exist
+    with pytest.raises(ValueError):
+        g.bfs(start_node, end="Nonexistent Node")
+
+    
 
